c7266aa2cc2be115623f439117e4d7ee9c6b47d0
Mohd Feroz Baig <feroz.baig96@gmail.com>
2016-10-02 23:39:47 +0530

    added validity checks

--- app/src/main/java/com/app/fbulou/inventory/MainActivity.java
+++ app/src/main/java/com/app/fbulou/inventory/MainActivity.java
@@ -1,7 +1,9 @@
 package com.app.fbulou.inventory;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -32,10 +34,32 @@ public class MainActivity extends AppCompatActivity {
         login_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               // startActivity(new Intent(MainActivity.this, AdminActivity.class));
+                validateForm();
             }
         });
     }
 
+    private void validateForm() {
+        Drawable d = ResourcesCompat.getDrawable(getResources(), R.drawable.ic_error_red_500_24dp, null);
+        assert d != null;
+        d.setBounds(0, 0, d.getIntrinsicWidth(), d.getIntrinsicHeight());
+
+
+        if (username.getText().length() == 0) {
+            username.setError("Username cannot be empty", d);
+            username.requestFocus();
+
+        } else if (password.getText().length() == 0) {
+            password.setError("Password cannot be empty", d);
+            password.requestFocus();
+
+        } else if (password.getText().length() < 8) {
+            password.setError("Minimum 8 characters required", d);
+            password.requestFocus();
+
+        } else
+            startActivity(new Intent(MainActivity.this, AdminActivity.class));
+
+    }
 
 }
